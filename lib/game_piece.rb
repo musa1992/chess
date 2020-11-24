89a64c2052dc5d1a26d4a046999044ada99e120c
musa <mosesbwire91@gmail.com>
2020-11-24 16:32:44 +0300

    Add queen class, Add method all possible move plus its helpers

--- lib/game_piece.rb
+++ lib/game_piece.rb
@@ -29,17 +29,52 @@ class King
 
 end
 
-k = King.new('black', 'bk', [0,4])
-z = King.new('black', 'bl', [5,4])
-a = King.new('black', 'bz', [1,4])
+class Queen
+    attr_reader :color, :unicode, :position
 
+    def initialize(color, unicode, position)
+        @color = color
+        @unicode = unicode
+        @position = position
+    end
 
-my_pieces = [k,z,a]
+    def all_possible_moves
+        x = [1,-1,0,0,-1,1,-1,1]
+        y = [0,0,1,-1,1,1,-1,-1]
+        moves = []
+        7.times do 
+            moves << all_possible_moves_helper(x,y)
+            x = generate_coords(x)
+            y = generate_coords(y) 
+        end
+        moves.delete_if {|elem| elem.flatten.empty?}
+        moves
+    end
+    def generate_coords(arr)
+        arr.map do |el|
+            if el.positive?
+                el + 1
+            elsif el.negative?
+                el + -1
+            else
+                el
+            end
+        end
+    end
 
-my_pieces.each do |piece|
-    print "#{piece.all_possible_moves} + #{piece.unicode}"
-    puts " "
+    def all_possible_moves_helper(x,y)
+        moves = []
+        8.times do |i|
+            moves << [position[0] + x[i], position[1] + y[i]]
+        end
+        moves.delete_if {|del| del.any?{|i| i < 0 || i > 7}}
+        moves        
+    end
 end
+
+q = Queen.new(" mo", "mo", [0,7])
+
+print q.all_possible_moves
 
 puts " "
 
