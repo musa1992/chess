aee18c4e27821a8c8610fa2f37e7369a5f38c2ae
musa <mosesbwire91@gmail.com>
2020-11-24 19:52:53 +0300

    Add GamePiece class - implements inheritance

--- lib/game_piece.rb
+++ lib/game_piece.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
 require 'colorize'
-class King
-  attr_reader :color, :unicode, :position
+class GamePiece
+    attr_reader :color, :unicode, :position
 
-  def initialize(color, unicode, position)
-    @color = color
-    @unicode = unicode
-    @position = position
-  end
-
+    def initialize(color, unicode, position)
+      @color = color
+      @unicode = unicode
+      @position = position
+    end
+    def move(pos)
+        update_position(pos) if all_possible_moves.any?(pos)
+    end
+    
+    def update_position(pos)
+        @position = pos
+    end 
+   
+end
+class King < GamePiece
+  
   def all_possible_moves
     x = [1, -1, 0, 0]
     y = [0, 0, 1, -1]
@@ -20,25 +30,10 @@ class King
     moves.delete_if { |del| del.any? { |i| i.negative? || i > 7 } }
     moves
   end
-
-  def move(pos)
-    update_position(pos) if all_possible_moves.any?(pos)
-  end
-
-  def update_position(pos)
-    @position = pos
-  end
 end
 
-class Queen
-  attr_reader :color, :unicode, :position
-
-  def initialize(color, unicode, position)
-    @color = color
-    @unicode = unicode
-    @position = position
-  end
-
+class Queen < GamePiece
+  
   def all_possible_moves
     x = [1, -1, 0, 0, -1, 1, -1, 1]
     y = [0, 0, 1, -1, 1, 1, -1, -1]
@@ -73,24 +68,10 @@ class Queen
     moves
   end
 
-  def move(pos)
-    update_position(pos) if all_possible_moves.any?(pos)
-  end
-
-  def update_position(pos)
-    @position = pos
-  end
 end
 
-class Rook
-    attr_reader :color, :unicode, :position
-
-    def initialize(color, unicode, position)
-      @color = color
-      @unicode = unicode
-      @position = position
-    end
-  
+class Rook < GamePiece
+    
     def all_possible_moves
       x = [1, -1, 0, 0]
       y = [0, 0, 1, -1]
@@ -123,25 +104,10 @@ class Rook
       end
       moves.delete_if { |del| del.any? { |i| i.negative? || i > 7 } }
       moves
-    end
-  
-    def move(pos)
-      update_position(pos) if all_possible_moves.any?(pos)
-    end
-  
-    def update_position(pos)
-      @position = pos
-    end
+    end    
 end
-class Bishop
-    attr_reader :color, :unicode, :position
-
-    def initialize(color, unicode, position)
-      @color = color
-      @unicode = unicode
-      @position = position
-    end
-  
+class Bishop < GamePiece
+    
     def all_possible_moves
       x = [-1, 1, -1, 1]
       y = [1, 1, -1, -1]
@@ -175,25 +141,10 @@ class Bishop
       moves.delete_if { |del| del.any? { |i| i.negative? || i > 7 } }
       moves
     end
-  
-    def move(pos)
-      update_position(pos) if all_possible_moves.any?(pos)
-    end
-  
-    def update_position(pos)
-      @position = pos
-    end
 end
 
-class Knight
-    attr_reader :color, :unicode, :position
-
-    def initialize(color, unicode, position)
-      @color = color
-      @unicode = unicode
-      @position = position
-    end
-  
+class Knight < GamePiece
+    
     def all_possible_moves
       x = [2,2,-2,-2,1,1,-1,-1]
       y = [1,-1,1,-1,2,-2,2,-2]
@@ -204,27 +155,17 @@ class Knight
       moves.delete_if { |del| del.any? { |i| i.negative? || i > 7 } }
       moves
     end
-  
-    def move(pos)
-      update_position(pos) if all_possible_moves.any?(pos)
-    end
-  
-    def update_position(pos)
-      @position = pos
-    end   
+   
 end
 
-class Pawn
-    attr_reader :color, :unicode, :position, :shift
-
+class Pawn < GamePiece
+    attr_reader :shift
     def initialize(color, unicode, position)
-      @color = color
-      @unicode = unicode
-      @position = position
-      @original_pos = position
-      @shift = shift_factor
+        super
+        @original_pos = position
+        @shift = shift_factor
     end
-
+      
     def shift_factor
         if @original_pos.first == 6
              -1
@@ -256,24 +197,29 @@ class Pawn
       return first_move if @original_pos == position
       moves
     end
-  
-    def move(pos)
-      update_position(pos) if all_possible_moves.any?(pos)
-    end
-  
-    def update_position(pos)
-      @position = pos
-    end
-
 end
 
-b = Pawn.new('mo','mo', [1,3])
+k = Pawn.new('m','w', [1,3])
 
-print b.all_possible_moves
-b.move([3,1])
+print k.all_possible_moves
+puts ' '
+print k.position
+
 puts ' '
 
-print b.all_possible_moves
+k.move([2,2])
+puts " after move"
+print k.all_possible_moves
+puts ' '
+print k.position
+
+puts ' '
+
+k.move([3,3])
+puts " after move"
+print k.all_possible_moves
+puts ' '
+print k.position
 
 puts ' '
 
